d85b8e486f6a4c65e6bd1dd16ec28ccab3c2969f
dwrekofc <153927388+dwrekofc@users.noreply.github.com>
2026-03-19 09:11:27 -0600

    chore: bump ralph to v0.3.1

--- Formula/ralph.rb
+++ Formula/ralph.rb
@@ -3,8 +3,8 @@ class Ralph < Formula
 
   desc "Bootstrap projects with the Ralph Playbook methodology"
   homepage "https://github.com/dwrekofc/ralph-playbook"
-  url "https://github.com/dwrekofc/ralph-playbook/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "91fc3ff1c9d100bc62bbf5465f1f700023a3665049c6db7e61f9344613bb7489"
+  url "https://github.com/dwrekofc/ralph-playbook/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "04ac702d559ca8b7ff7e914c13a186416a205c8a8b16b7d8bfb75740535a11cb"
 
   depends_on "python@3.12"
 

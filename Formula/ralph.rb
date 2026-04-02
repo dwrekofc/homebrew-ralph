3001678a2eaa98d38406109abf42801494c56571
dwrekofc <derek@seshling.com>
2026-04-02 17:07:59 -0600

    bump ralph to v0.5.0-beta

--- Formula/ralph.rb
+++ Formula/ralph.rb
@@ -3,8 +3,8 @@ class Ralph < Formula
 
   desc "AI development framework scaffolding and management"
   homepage "https://github.com/dwrekofc/ralph-playbook"
-  url "https://github.com/dwrekofc/ralph-playbook/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "4b938d771bc2e8b49e0b9d66076bad4b2809ea0c5405dcf4226b44c757096796"
+  url "https://github.com/dwrekofc/ralph-playbook/archive/refs/tags/v0.5.0-beta.tar.gz"
+  sha256 "21aa5e5910f4b1bceb4f3c69ebbf02dbcedb07b617071d6c6e8cc89eeabf585f"
 
   depends_on "python@3.12"
 
